5980024552793791a5362ec22aa80c1c2f93b53a
HaLimau <yetanotherakcount@gmail.com>
2024-01-09 05:01:28 +0800

    aa

--- menus/menu.php
+++ menus/menu.php
@@ -1,11 +1,34 @@
 <nav>
         <div class="nav-left">
-            <a href="login.php">Home</a>
-            <a href="login.php">Library Books</a>
-            <a href="login.php">Print</a>
-            <a href="login.php">News</a>
-            <a href="login.php">Library Room Booking</a>
-            <a href="login.php">Home - Login</a>
+            <a href="index.php">Home</a>
             
+            <div class="subnav">
+                <button class="subnavBtn">Library Books<i class="fa-solid fa-caret-down"></i></button>
+                <div class="subnav-content">
+                    <a href="#">Collection</a>
+                    <a href="print_status.php">My Reads</a>
+                    <a href="#">Empty for now Delete if no use</a>
+                </div>
+            </div>
+            
+            <div class="subnav">
+                <button class="subnavBtn">Files <i class="fa-solid fa-caret-down"></i></button>
+                <div class="subnav-content">
+                    <a href="file_upload.php">Upload File</a>
+                    <a href="file_storage.php">Manage Files</a>
+                    <a href="#">Empty for now Delete if no use</a>
+                </div>
+            </div>
+            <a href="print_history.php">History</a>
+            <div class="subnav">
+                <button class="subnavBtn">Account <i class="fa-solid fa-caret-down"></i></button>
+                <div class="subnav-content">
+                    <a href="#">Setttings</a>
+                    <a href="#">Empty for now Delete if no use</a>
+                </div>
+            </div>
+        </div>
+        <div class="nav-right">
+            <a href="logout.php" class="logout">Logout</a>
         </div>
     </nav>
--- menus/menunav.php
+++ menus/menunav.php
@@ -1,32 +1,13 @@
+
     <nav>
         <div class="nav-left">
             <a href="index.php">Home</a>
+            <a href="books.php">Library Books</a>
+            <a href="print.php">Print</a>
+            <a href="news.php">News</a>
+            <a href="room_booking.php">Library Room Booking</a>
+            <a href="profile.php">Profile</a>
             
-            <div class="subnav">
-                <button class="subnavBtn">Library Books<i class="fa-solid fa-caret-down"></i></button>
-                <div class="subnav-content">
-                    <a href="#">Collection</a>
-                    <a href="print_status.php">My Reads</a>
-                    <a href="#">Empty for now Delete if no use</a>
-                </div>
-            </div>
-            
-            <div class="subnav">
-                <button class="subnavBtn">Files <i class="fa-solid fa-caret-down"></i></button>
-                <div class="subnav-content">
-                    <a href="file_upload.php">Upload File</a>
-                    <a href="file_storage.php">Manage Files</a>
-                    <a href="#">Empty for now Delete if no use</a>
-                </div>
-            </div>
-            <a href="print_history.php">History</a>
-            <div class="subnav">
-                <button class="subnavBtn">Account <i class="fa-solid fa-caret-down"></i></button>
-                <div class="subnav-content">
-                    <a href="#">Setttings</a>
-                    <a href="#">Empty for now Delete if no use</a>
-                </div>
-            </div>
         </div>
         <div class="nav-right">
             <a href="logout.php" class="logout">Logout</a>
